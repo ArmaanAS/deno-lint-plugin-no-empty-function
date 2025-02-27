d2b24f81fbcf096b2fe447d2e38014ecc663b4dd
Armaan S <armaanrsalam@gmail.com>
2025-02-27 21:59:25 +0000

    Refactor plugin to use selectors

--- plugin.ts
+++ plugin.ts
@@ -4,24 +4,6 @@ const plugin: Deno.lint.Plugin = {
   rules: {
     "no-empty-function": {
       create(context) {
-        function isBodyEmpty(body: Deno.lint.BlockStatement | null): boolean {
-          return !!body && body.type === "BlockStatement" &&
-            body.body.length === 0;
-        }
-        function isInXetter(node: Deno.lint.FunctionExpression): boolean {
-          const ancestors = context.sourceCode.getAncestors(node);
-          if (ancestors.length > 0) {
-            const parent = ancestors[ancestors.length - 1];
-            if (
-              (parent.type === "Property" ||
-                parent.type === "MethodDefinition") &&
-              (parent.kind === "set" || parent.kind === "get")
-            ) {
-              return true;
-            }
-          }
-          return false;
-        }
         function containsComment(node: Deno.lint.BlockStatement): boolean {
           const text = context.sourceCode.text.slice(
             node.range[0],
@@ -31,38 +13,31 @@ const plugin: Deno.lint.Plugin = {
           return text.includes("/*") || text.includes("//");
         }
         return {
-          FunctionDeclaration(node) {
-            if (isBodyEmpty(node.body) && !containsComment(node.body!)) {
-              context.report({
-                node: node.body!,
-                message: "Empty function body",
-                hint: "Add code or comment to the empty function body",
-              });
-            }
+          "FunctionDeclaration > BlockStatement[body.length=0]"(node) {
+            if (containsComment(node)) return;
+            context.report({
+              node,
+              message: "Empty function body",
+              hint: "Add code or comment to the empty function body",
+            });
           },
-          FunctionExpression(node) {
-            if (isInXetter(node)) return;
-            if (isBodyEmpty(node.body) && !containsComment(node.body!)) {
-              context.report({
-                node: node.body,
-                message: "Empty function body",
-                hint: "Add code or comment to the empty function body",
-              });
-            }
+          ":not(MethodDefinition[kind='get'], MethodDefinition[kind='set'], Property[kind='get'], Property[kind='set']) > FunctionExpression > BlockStatement[body.length=0]"(
+            node,
+          ) {
+            if (containsComment(node)) return;
+            context.report({
+              node,
+              message: "Empty function body",
+              hint: "Add code or comment to the empty function body",
+            });
           },
-          ArrowFunctionExpression(node) {
-            if (
-              node.body &&
-              node.body.type === "BlockStatement" &&
-              node.body.body.length === 0 &&
-              !containsComment(node.body)
-            ) {
-              context.report({
-                node: node.body,
-                message: "Empty arrow function body",
-                hint: "Add code or comment to the empty arrow function body",
-              });
-            }
+          "ArrowFunctionExpression > BlockStatement[body.length=0]"(node) {
+            if (containsComment(node)) return;
+            context.report({
+              node,
+              message: "Empty arrow function body",
+              hint: "Add code or comment to the empty arrow function body",
+            });
           },
         };
       },
