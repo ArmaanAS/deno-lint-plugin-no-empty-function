54b50e729432e4c29689915aca1b9e2b162ef5f0
Armaan S <armaanrsalam@gmail.com>
2025-02-27 22:43:29 +0000

    Shorten selector

--- plugin.ts
+++ plugin.ts
@@ -5,15 +5,12 @@ const plugin: Deno.lint.Plugin = {
     "no-empty-function": {
       create(context) {
         function containsComment(node: Deno.lint.BlockStatement): boolean {
-          const text = context.sourceCode.text.slice(
-            node.range[0],
-            node.range[1],
-          );
+          const text = context.sourceCode.getText(node);
 
           return text.includes("/*") || text.includes("//");
         }
         return {
-          "FunctionDeclaration > BlockStatement[body.length=0], :not(MethodDefinition[kind='get'], MethodDefinition[kind='set'], Property[kind='get'], Property[kind='set']) > FunctionExpression > BlockStatement[body.length=0], ArrowFunctionExpression > BlockStatement[body.length=0]"(
+          ":not([kind=get], [kind=set]) > :not(:not(FunctionDeclaration, FunctionExpression, ArrowFunctionExpression)) > BlockStatement[body.length=0]"(
             node,
           ) {
             if (containsComment(node)) return;
